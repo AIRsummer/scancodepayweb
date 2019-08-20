dd3e13b73a3bc5e87352745581d6f1a9e46de28f
mike.liao <liao.yu@foocaa.com>
2019-08-20 09:56:01 +0800

    测试组件

--- scancodepayweb/src/App.js
+++ scancodepayweb/src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Test from './test.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor (pros) {
+    super(pros)
+    this.state = {
+      var:"mike"
+    }
+  }
+  render() {
+    return (
+        <div className="App">
+          {/*<header className="App-header">*/}
+          {/*  <img src={logo} className="App-logo" alt="logo" />*/}
+          {/*  <p>*/}
+          {/*    Edit <code>src/App.js</code> and save to reload.*/}
+          {/*  </p>*/}
+          {/*  <a*/}
+          {/*    className="App-link"*/}
+          {/*    href="https://reactjs.org"*/}
+          {/*    target="_blank"*/}
+          {/*    rel="noopener noreferrer"*/}
+          {/*  >*/}
+          {/*    Learn React*/}
+          {/*  </a>*/}
+          {/*</header>*/}
+          <a>hello</a>
+          {this.state.var}
+          <Test></Test>
+        </div>
+    );
+  }
 }
 
 export default App;
