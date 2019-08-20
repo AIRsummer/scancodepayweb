1717c8ef1521a6e71d7b30907949d427e5bbc6f8
mike.liao <liao.yu@foocaa.com>
2019-08-20 10:26:20 +0800

    测试组件 点击事件

---
+++ scancodepayweb/src/test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import App from "./App";
+
+function Test() {
+    return (
+        <div className="Test">
+            <a>测试一个组件</a>
+        </div>
+    );
+}
+export default Test;
